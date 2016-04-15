5b84577d2329fc27f1a0d334c66455e26da11787
koralm <koralm.for@gmail.com>
2016-04-15 08:14:46 +0200

    from lap

--- routes/demo.js
+++ routes/demo.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('demo/demo', { title: 'Lubuska sieć Innowacji - demo' });
 });
 
+router.get('/dodaj', function(req, res, next) {
+    res.render('demo/d_dodaj', { title: 'Lubuska sieć Innowacji - demo' });
+});
+
 module.exports = router;
